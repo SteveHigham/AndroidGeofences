df4d976891493fb502344fdb4616aa1ad69d7e68
Steve Higham (iMac) <steve@sjlt.co.uk>
2020-03-07 17:15:14 +0000

    First cut. App is running and creates a Geofencing client.

---
+++ app/src/main/java/uk/co/sjlt/androidgeofences/MainActivity.java
@@ -0,0 +1,26 @@
+package uk.co.sjlt.androidgeofences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity
+{
+
+private GeofencingClient geofencingClient;
+
+@Override
+protected void onCreate (Bundle savedInstanceState)
+{
+  super.onCreate (savedInstanceState);
+  setContentView (R.layout.activity_main);
+  geofencingClient = LocationServices.getGeofencingClient(this);
+}
+
+}
+
+// End of class
+
