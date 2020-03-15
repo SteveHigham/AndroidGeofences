3984ef47eaedfafd5f9896a992f3b6285e8f7076
Steve Higham (iMac) <steve@sjlt.co.uk>
2020-03-15 22:52:37 +0000

    Back button now displaying and working on show events toolbar.

--- app/src/main/java/uk/co/sjlt/androidgeofences/FenceEventsActivity.java
+++ app/src/main/java/uk/co/sjlt/androidgeofences/FenceEventsActivity.java
@@ -21,16 +21,8 @@ protected void onCreate (Bundle savedInstanceState)
   Toolbar toolbar = findViewById (R.id.toolbar);
   setSupportActionBar (toolbar);
 
-  FloatingActionButton fab = findViewById (R.id.fab);
-  fab.setOnClickListener (new View.OnClickListener ()
-  {
-    @Override
-    public void onClick (View view)
-    {
-      Snackbar.make (view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction ("Action", null).show ();
-    }
-  });
+  getSupportActionBar().setDisplayHomeAsUpEnabled (true);
+  getSupportActionBar().setDisplayShowHomeEnabled (true);
 }
 
 }
