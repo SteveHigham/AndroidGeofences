5c2a31652295d19b0758feb22720d2140b659b2d
Steve Higham (iMac) <steve@sjlt.co.uk>
2020-03-16 18:15:14 +0000

    This approach seems to be working. See: https://code.tutsplus.com/tutorials/ getting-started-with-recyclerview-and-cardview-on-android--cms-23465

---
+++ app/src/main/java/uk/co/sjlt/androidgeofences/FenceEventsAdapter.java
@@ -0,0 +1,43 @@
+package uk.co.sjlt.androidgeofences;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FenceEventsAdapter extends RecyclerView.Adapter<FenceEventsAdapter.ViewHolder>
+{
+
+public class ViewHolder extends RecyclerView.ViewHolder
+{
+  public ViewHolder (View view)
+  {
+    super (view);
+  }
+}
+
+@NonNull
+@Override
+public ViewHolder onCreateViewHolder (@NonNull ViewGroup parent, int viewType)
+{
+  return null;
+}
+
+@Override
+public void onBindViewHolder (@NonNull ViewHolder holder, int position)
+{
+
+}
+
+@Override
+public int getItemCount ()
+{
+  return 0;
+}
+
+}
+
+// End of class
+
+
