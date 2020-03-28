539bf1a81f5661fae48f6c9db7076fdcd2acbd45
Steve Higham (iMac) <steve@sjlt.co.uk>
2020-03-28 22:40:38 +0000

    I'm now showing a blank map. I think this is because Google Play is not installed on the emulator. See https://medium.com/@dai_shi/ installing-google-play-services-on-an-android-studio-emulator -fffceb2c28a1

--- app/src/main/java/uk/co/sjlt/androidgeofences/ShowLocationActivity.java
+++ app/src/main/java/uk/co/sjlt/androidgeofences/ShowLocationActivity.java
@@ -6,8 +6,13 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * An activity that displays a Google map with a marker (pin) to indicate a
@@ -26,14 +31,30 @@ private Location displayLocation;
 protected void onCreate (Bundle savedInstanceState)
 {
   super.onCreate (savedInstanceState);
+
+  // Get the location to display and log it
   displayLocation = getIntent ().getParcelableExtra ("Location");
   Log.v ( Constants.LOGTAG, CLASSTAG + "onCreate called with Location: " +
       displayLocation );
+
+  setContentView (R.layout.activity_show_location);
+  SupportMapFragment fragment =
+      (SupportMapFragment) getSupportFragmentManager ()
+          .findFragmentById (R.id.map);
+  fragment.getMapAsync (this);
 }
 
 @Override
 public void onMapReady (GoogleMap googleMap)
 {
+  Log.v ( Constants.LOGTAG, CLASSTAG + "onMapReady called for Location: " +
+      displayLocation );
+  LatLng loc = new LatLng ( displayLocation.getLatitude (),
+      displayLocation.getLongitude () );
+  MarkerOptions options = new MarkerOptions ().position (loc).title ("Location");
+  googleMap.addMarker (options);
+  CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng (loc);
+  googleMap.moveCamera (cameraUpdate);
 }
 
 }
