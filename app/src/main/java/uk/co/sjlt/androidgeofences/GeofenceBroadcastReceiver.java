6bf6b5513fb23f031f225d8f15c686e7d3ce83d5
Steve Higham (iMac) <steve@sjlt.co.uk>
2020-04-15 22:13:37 +0100

    Notifications are basically working but some strange behaviour. Need to look at the intent flags in the BroadcastReceiver.

--- app/src/main/java/uk/co/sjlt/androidgeofences/GeofenceBroadcastReceiver.java
+++ app/src/main/java/uk/co/sjlt/androidgeofences/GeofenceBroadcastReceiver.java
@@ -1,11 +1,15 @@
 package uk.co.sjlt.androidgeofences;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Message;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
@@ -16,6 +20,8 @@ import java.util.List;
 
 import uk.co.sjlt.androidgeofences.model.FenceEvent;
 import uk.co.sjlt.androidgeofences.ui.DisplayLocationActivity;
+import uk.co.sjlt.androidgeofences.ui.FenceEventsActivity;
+import uk.co.sjlt.androidgeofences.ui.RequestPermissionsActivity;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver
 {
@@ -65,8 +71,62 @@ private void handleTransition (Context context, String fence, int transition)
       app.getTransitionString (resources, transition), fence );
   Log.i (Constants.LOGTAG, CLASSTAG + msg);
 
-  // Add to the event list
+  // Add to the event list and raise a notification
   app.getEvents ().add (new FenceEvent (new Date (), fence, transition));
+  raiseNotification (context, fence, transition, resources);
+}
+
+private void raiseNotification ( Context context, String fence,
+                                 int transition, Resources resources )
+{
+  Log.v ( Constants.LOGTAG, CLASSTAG +
+      "Raise a notification for crossing fence: " + fence );
+
+  // Create the Intent to be launched from the notification
+  Intent intent = new Intent (context, FenceEventsActivity.class);
+  intent.setFlags ( Intent.FLAG_ACTIVITY_NEW_TASK |
+      Intent.FLAG_ACTIVITY_CLEAR_TASK );
+  PendingIntent pendingIntent =
+      PendingIntent.getActivity (context, 0, intent, 0);
+
+  // Create the builder and trigger the notification
+  String title =
+      resources.getString (R.string.receiver_transition_notification_title);
+  String text;
+  switch (transition)
+  {
+    case Geofence.GEOFENCE_TRANSITION_ENTER:
+      text = resources.getString (
+          R.string.receiver_transition_notification_entry, fence );
+      break;
+    case Geofence.GEOFENCE_TRANSITION_EXIT:
+      text = resources.getString (
+          R.string.receiver_transition_notification_exit, fence );
+      break;
+    default:
+      Log.w ( Constants.LOGTAG, CLASSTAG + "Unknown transition code " +
+          transition + " on fence " + fence );
+      text = resources.getString (
+          R.string.receiver_transition_notification_unknown,
+          transition, fence );
+  }
+  NotificationCompat.Builder builder =
+      new NotificationCompat.Builder ( context,
+          RequestPermissionsActivity.CHANNEL_ID )
+      .setSmallIcon (R.drawable.ic_launcher_foreground)
+      .setContentTitle (title)
+      .setContentText (text)
+      /*
+      .setStyle(new NotificationCompat.BigTextStyle()
+          .bigText("Much longer text that cannot fit one line..."))
+       */
+      .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+      .setContentIntent (pendingIntent)
+      .setAutoCancel (true);
+  NotificationManagerCompat notificationManager =
+      NotificationManagerCompat.from (context);
+  // notificationId (1) is a unique int for each notification that you must define
+  notificationManager.notify (1, builder.build ());
 }
 
 }
--- app/src/main/java/uk/co/sjlt/androidgeofences/ui/RequestPermissionsActivity.java
+++ app/src/main/java/uk/co/sjlt/androidgeofences/ui/RequestPermissionsActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
@@ -42,6 +44,9 @@ private static final String CLASSTAG =
     " " + RequestPermissionsActivity.class.getSimpleName () + " ";
 private static final int REQUEST_CODE_PERMISSIONS = 101;
 
+// For notifications
+public static final String CHANNEL_ID = "FENCE_NOTIFICATION";
+
 private Button btnPermissions;
 
 /**
@@ -57,7 +62,10 @@ protected void onCreate (Bundle savedInstanceState)
   Log.v (Constants.LOGTAG, CLASSTAG + "onCreate called");
   setContentView (R.layout.activity_request_permissions);
 
-  // First priority is to check permissions. If we already have all the
+  // First we set up notifications
+  createNotificationChannel ();
+
+  // Second priority is to check permissions. If we already have all the
   // permissions we desire then no user interaction is required.
   final List<String> permissions = permissionsDesired ();
 
@@ -191,6 +199,25 @@ private void createInadequatePermissionsDialog ()
         }
       })
       .create ();
+}
+
+private void createNotificationChannel ()
+{
+  // Create the NotificationChannel, but only on API 26+ because
+  // the NotificationChannel class is new and not in the support library
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+  {
+    CharSequence name = getString (R.string.channel_name);
+    int importance = NotificationManager.IMPORTANCE_DEFAULT;
+    NotificationChannel channel =
+        new NotificationChannel (CHANNEL_ID, name, importance);
+    channel.setDescription (getString (R.string.channel_description));
+    // Register the channel with the system; you can't change the importance
+    // or other notification behaviors after this
+    NotificationManager notificationManager =
+        getSystemService (NotificationManager.class);
+    notificationManager.createNotificationChannel (channel);
+  }
 }
 
 private void handleAllLocationUpdates (boolean coarsePermission, boolean finePermission)
